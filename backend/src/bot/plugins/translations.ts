5bffaa966c4ce020f13cd1894a269d183a4694e0
evermake <vladeryabkin@gmail.com>
2024-03-10 14:32:17 +0300

    feat(bot): choose translation based on user's language

--- backend/src/bot/plugins/translations.ts
+++ backend/src/bot/plugins/translations.ts
@@ -1,3 +1,4 @@
+import type { DomainFlavor } from './domain'
 import type { InstallFn } from '.'
 import type { Translation } from '~/translations'
 import translations from '~/translations'
@@ -6,10 +7,20 @@ export type TranslationsFlavor = {
   t: Translation
 }
 
-export const install: InstallFn<TranslationsFlavor> = (bot) => {
+export const install: InstallFn<TranslationsFlavor & DomainFlavor> = (bot) => {
+  const getTranslationOrDefault = (language: string) => (
+    translations[language as keyof typeof translations]
+    || translations.en
+  )
+
   bot.use((ctx, next) => {
-    // @todo Use user's preferred language.
-    ctx.t = translations.en
+    if (ctx.user?.language) {
+      ctx.t = getTranslationOrDefault(ctx.user.language)
+    } else if (ctx.from?.language_code) {
+      ctx.t = getTranslationOrDefault(ctx.from.language_code)
+    } else {
+      ctx.t = translations.en
+    }
     return next()
   })
 }

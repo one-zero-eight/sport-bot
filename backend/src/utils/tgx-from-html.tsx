acae7f764142bb6129a0762d9be9c77af1287c68
evermake <vladeryabkin@gmail.com>
2024-03-26 21:14:39 +0300

    chore: remove commented code

--- backend/src/utils/tgx-from-html.tsx
+++ backend/src/utils/tgx-from-html.tsx
@@ -40,10 +40,3 @@ function tgxFromNode(node: ChildNode): TgxElement {
   }
   return <></>
 }
-
-// console.log(
-//   inspect(
-//     tgxFromHtml(`<p>The club trains in the following areas: hiking, historical fencing, folk games and general physical training. For self-development it is desirable to attend at least 2 classes a week.</p>\r\n<p>&nbsp;</p>\r\n<p>We also organize events, such as flying in a hot air tube, origami folding or traditional tea parties.</p>\r\n<p>&nbsp;</p>\r\n<p>Hiking</p>\r\n<p>Monday 19:30 - 21:30 - In front of SC</p>\r\n<p>We are walking along the outskirts of Innopolis on rough terrain.</p>\r\n<p>&nbsp;</p>\r\n<p>Historical Fencing</p>\r\n<p>Tuesday 16:30 - 18:30 - Big hall (RAGE Knights)</p>\r\n<p>Thursday 17:00 - 18:30 - Big hall (RAGE Knights)</p>\r\n<p>We train in the art of swordsmanship with a one-handed sword and shield. This is where you get praise for beating a man with a stick)</p>\r\n<p>&nbsp;</p>\r\n<p>Folk Games</p>\r\n<p>Wednesday 19:30 - 21:30 - Outdoor Basketball court</p>\r\n<p>We are playing in a lot of games, which can play both girls and boys, like Lapta, or hide-and-seek, salki and so on.</p>\r\n<p>&nbsp;</p>\r\n<p>Workout</p>\r\n<p>Friday 21:00 - 22:30 - 232 SC</p>\r\n<p>General physical training. Working with your own weight and sometimes with a partner. Mostly circular training sessions.</p>\r\n<p>&nbsp;</p>\r\n<p>Club Fee</p>\r\n<p>For buying equipment and developing the club we are gathering club fee. 500 rub per month. First month is free.</p>\r\n<p>&nbsp;</p>\r\n<p>Telegram channels</p>\r\n<p><a href=\"https://t.me/dich_trainings\">General</a></p>\r\n<p><a href=\"https://t.me/rage_knights\">RAGE Knights</a></p>`),
-//     { depth: null },
-//   ),
-// )

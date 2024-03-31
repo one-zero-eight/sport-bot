11eafea3589ebb46fb574679781fa81914ec0ae9
evermake <vladeryabkin@gmail.com>
2024-03-31 11:16:32 +0300

    feat: run bot with grammY runner, add graceful shutdown

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,5 @@
+import process from 'node:process'
+import { run } from '@grammyjs/runner'
 import { loadConfigFromEnv } from './config'
 import { createLogger } from './utils/logging'
 import { createBot } from './bot'
@@ -13,15 +15,17 @@ async function main() {
       : { level: 'info', pretty: false },
   )
 
+  const db = await createDatabase({
+    logger: logger.child({ tag: 'database' }),
+    connectionUrl: config.postgresUrl,
+  })
+
   const bot = createBot({
     logger: logger.child({ tag: 'bot' }),
     token: config.bot.token,
     domain: new Domain({
       logger: logger.child({ tag: 'domain' }),
-      db: await createDatabase({
-        logger: logger.child({ tag: 'database' }),
-        connectionUrl: config.postgresUrl,
-      }),
+      db: db,
       sport: new SportClient({
         logger: logger.child({ tag: 'sport' }),
         baseUrl: config.sport.apiBaseUrl,
@@ -30,7 +34,19 @@ async function main() {
     }),
   })
 
-  await bot.start()
+  await bot.init()
+  logger.info({ msg: `Starting bot https://t.me/${bot.botInfo.username}` })
+
+  const runner = run(bot)
+
+  const stop = () => {
+    runner.stop()
+    db.$disconnect()
+  }
+  process.once('SIGINT', stop)
+  process.once('SIGTERM', stop)
+
+  await runner.task()
 }
 
 main()

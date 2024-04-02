f1fde8d20d95bea6c6d379b683810a78d254ee44
evermake <vladeryabkin@gmail.com>
2024-04-02 13:14:23 +0300

    refactor `dates` utilities, add `Day` class, update tests

--- backend/src/utils/dates.test.ts
+++ backend/src/utils/dates.test.ts
@@ -1,9 +1,6 @@
 import { describe, expect, it } from 'vitest'
-import {
-  getDateDayInTimezone,
-  getDayBoundaries,
-  getTimezoneOffset,
-} from './dates'
+import type { Weekday } from './dates'
+import { Day, getTimezoneOffset } from './dates'
 
 describe('getTimezoneOffset', () => {
   it('returns correct offset for Europe/Moscow', () => {
@@ -14,45 +11,102 @@ describe('getTimezoneOffset', () => {
   })
 })
 
-describe('getDateDayInTimezone', () => {
-  it('returns correct date for 17/03/2024 in Moscow timezone', () => {
-    expect(
-      getDateDayInTimezone(new Date('2024-03-17T22:03:00Z'), 'Europe/Moscow'),
-    ).toEqual({ year: 2024, month: 3, day: 18 })
+describe('day', () => {
+  it('creates correct day from string', () => {
+    let day = Day.fromString('2024-03-17')
+    expect(day.year).toBe(2024)
+    expect(day.month).toBe(3)
+    expect(day.date).toBe(17)
+
+    day = Day.fromString('2024-02-29')
+    expect(day.year).toBe(2024)
+    expect(day.month).toBe(2)
+    expect(day.date).toBe(29)
   })
 
-  it('returns correct date for 17/03/2024 in UTC timezone', () => {
-    expect(
-      getDateDayInTimezone(new Date('2024-03-17T22:03:00Z'), 'UTC'),
-    ).toEqual({ year: 2024, month: 3, day: 17 })
-  })
-})
-
-describe('getDayBoundaries', () => {
-  it('returns correct boundaries for 17/03/2024 in Moscow timezone', () => {
-    expect(
-      getDayBoundaries({
-        year: 2024,
-        month: 3,
-        day: 17,
-        timezone: 'Europe/Moscow',
-      }),
-    ).toEqual([
-      new Date('2024-03-16T21:00:00Z'),
-      new Date('2024-03-17T21:00:00Z'),
-    ])
+  it('converts to string correctly', () => {
+    expect(Day.fromString('2024-03-17').toString()).toBe('2024-03-17')
+    expect(Day.fromString('2024-02-29').toString()).toBe('2024-02-29')
   })
 
-  it('returns correct boundaries for 17/03/2024 in UTC timezone', () => {
-    expect(
-      getDayBoundaries({
-        year: 2024,
-        month: 3,
-        day: 17,
-      }),
-    ).toEqual([
-      new Date('2024-03-17T00:00:00Z'),
-      new Date('2024-03-18T00:00:00Z'),
-    ])
+  it('creates correct day from date and timezone', () => {
+    const date = new Date('2024-03-17T22:03:00Z')
+    let day = Day.fromDate(date, 'UTC')
+    expect(day.year).toBe(2024)
+    expect(day.month).toBe(3)
+    expect(day.date).toBe(17)
+
+    day = Day.fromDate(date, 'Europe/Moscow')
+    expect(day.year).toBe(2024)
+    expect(day.month).toBe(3)
+    expect(day.date).toBe(18)
+  })
+
+  it('converts to date with timezone correctly', () => {
+    let date = Day.fromString('2024-03-17').asDate('UTC')
+    expect(date.toISOString()).toBe('2024-03-17T00:00:00.000Z')
+
+    date = Day.fromString('2024-03-17').asDate('Europe/Moscow')
+    expect(date.toISOString()).toBe('2024-03-17T03:00:00.000Z')
+  })
+
+  it('returns correct weekday', () => {
+    const testcases: [string, Weekday][] = [
+      ['2024-01-01', 'mon'],
+      ['2024-01-02', 'tue'],
+      ['2024-01-03', 'wed'],
+      ['2024-01-04', 'thu'],
+      ['2024-01-05', 'fri'],
+      ['2024-01-06', 'sat'],
+      ['2024-01-07', 'sun'],
+
+      ['2024-04-01', 'mon'],
+      ['2024-04-02', 'tue'],
+      ['2024-04-03', 'wed'],
+      ['2024-04-04', 'thu'],
+      ['2024-04-05', 'fri'],
+      ['2024-04-06', 'sat'],
+      ['2024-04-07', 'sun'],
+
+      ['2024-12-30', 'mon'],
+      ['2024-12-31', 'tue'],
+      ['2025-01-01', 'wed'],
+      ['2025-01-02', 'thu'],
+      ['2025-01-03', 'fri'],
+      ['2025-01-04', 'sat'],
+      ['2025-01-05', 'sun'],
+    ]
+    for (const [dateStr, weekday] of testcases) {
+      expect(Day.fromString(dateStr).weekday).toBe(weekday)
+    }
+  })
+
+  it('shifts correctly', () => {
+    const testcases: [string, number, string][] = [
+      ['2024-01-01', -3, '2023-12-29'],
+      ['2024-01-01', -2, '2023-12-30'],
+      ['2024-01-01', -1, '2023-12-31'],
+      ['2024-01-01', 0, '2024-01-01'],
+      ['2024-01-01', 1, '2024-01-02'],
+      ['2024-01-01', 2, '2024-01-03'],
+      ['2024-01-01', 3, '2024-01-04'],
+    ]
+    for (const [dateStr, shift, expectedStr] of testcases) {
+      expect(Day.fromString(dateStr).shift(shift).toString()).toBe(expectedStr)
+    }
+  })
+
+  it('returns correct boundaries', () => {
+    const testcases: [string, string, [string, string]][] = [
+      ['2024-03-17', 'UTC', ['2024-03-17T00:00:00.000Z', '2024-03-18T00:00:00.000Z']],
+      ['2024-02-29', 'UTC', ['2024-02-29T00:00:00.000Z', '2024-03-01T00:00:00.000Z']],
+      ['2024-03-17', 'Europe/Moscow', ['2024-03-16T21:00:00.000Z', '2024-03-17T21:00:00.000Z']],
+      ['2024-02-29', 'Europe/Moscow', ['2024-02-28T21:00:00.000Z', '2024-02-29T21:00:00.000Z']],
+    ]
+    for (const [dateStr, timezone, [startStr, endStr]] of testcases) {
+      const [start, end] = Day.fromString(dateStr).boundaries(timezone)
+      expect(start.toISOString()).toBe(startStr)
+      expect(end.toISOString()).toBe(endStr)
+    }
   })
 })

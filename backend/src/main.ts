c597f951cf8cc4c92135e15a2f52514067186d5c
evermake <vladeryabkin@gmail.com>
2024-04-08 20:13:47 +0300

    add bot's commands and descriptions, add option to run `setMy...` commands

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,7 @@
 import process from 'node:process'
+import { program } from 'commander'
 import { run } from '@grammyjs/runner'
+import { Bot } from 'grammy'
 import { loadConfigFromEnv } from './config'
 import { createLogger } from './utils/logging'
 import { createBot } from './bot'
@@ -7,8 +9,56 @@ import { Domain } from './domain'
 import { createDatabase } from './services/database'
 import { SportClient } from './services/sport'
 import { InnohassleClient } from './services/innohassle'
+import translations from './translations'
 
 async function main() {
+  program
+    .option('--set-my', 'Only call bot\'s `setMy...` methods.', false)
+
+  program.parse()
+
+  const options = program.opts()
+
+  if (options.setMy) {
+    await setMy()
+  } else {
+    runBot()
+  }
+}
+
+async function setMy() {
+  const config = loadConfigFromEnv()
+  const logger = createLogger(
+    config.environment === 'development'
+      ? { level: 'debug', pretty: true }
+      : { level: 'info', pretty: false },
+  )
+  const bot = new Bot(config.bot.token)
+
+  logger.info('setMyCommands(default)...')
+  await bot.api.setMyCommands(
+    Object.entries(translations.en['Bot.Commands']).map(([command, description]) => ({ command, description })),
+    { scope: { type: 'all_private_chats' } },
+  )
+  logger.info('setMyDescription(default)...')
+  await bot.api.setMyDescription(translations.en['Bot.About'])
+  logger.info('setMyShortDescription(default)...')
+  await bot.api.setMyShortDescription(translations.en['Bot.Bio'])
+
+  for (const [language, translation] of Object.entries(translations).filter(([lang]) => lang !== 'en')) {
+    logger.info(`setMyCommands(${language})...`)
+    await bot.api.setMyCommands(
+      Object.entries(translation['Bot.Commands']).map(([command, description]) => ({ command, description })),
+      { scope: { type: 'all_private_chats' }, language_code: language },
+    )
+    logger.info(`setMyDescription(${language})...`)
+    await bot.api.setMyDescription(translation['Bot.About'], { language_code: language })
+    logger.info(`setMyShortDescription(${language})...`)
+    await bot.api.setMyShortDescription(translation['Bot.Bio'], { language_code: language })
+  }
+}
+
+async function runBot() {
   const config = loadConfigFromEnv()
   const logger = createLogger(
     config.environment === 'development'

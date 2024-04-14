9ee879a63e768437c79376e957c4948b8896e2f5
evermake <vladeryabkin@gmail.com>
2024-04-14 22:10:10 +0300

    chore: send main menu when clicking outdated button

--- backend/src/bot/handlers/fallback-callback-query.ts
+++ backend/src/bot/handlers/fallback-callback-query.ts
@@ -1,4 +1,5 @@
 import { Composer } from 'grammy'
+import views from './views'
 import type { Ctx } from '~/bot/context'
 
 const composer = new Composer<Ctx>()
@@ -9,6 +10,12 @@ composer.on('callback_query:data', async (ctx) => {
     show_alert: true,
     cache_time: 60 * 5,
   })
+
+  if (ctx.msg?.from) {
+    await ctx
+      .send(await views.main.render(ctx, {}))
+      .to(ctx.from.id)
+  }
 })
 
 export default composer
